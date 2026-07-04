0262ec95b9a9573acfa67cb0feb29f414cd8282e
adarsh-appmaster <75722975+adarsh-appmaster@users.noreply.github.com>
2026-07-04 23:29:21 +0530

    ok change the vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5002',
+        target: 'https://a1deal-og-server.onrender.com',
         changeOrigin: true,
       },
     },
