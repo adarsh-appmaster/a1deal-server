9bb5f358927eecb108358f9fc29b83b4596cd3ac
adarsh-appmaster <75722975+adarsh-appmaster@users.noreply.github.com>
2026-07-04 23:26:10 +0530

    check

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ||'https://a1deal-og-server.onrender.com/api' ||'http://localhost:5002/api',
+  baseURL: import.meta.env.VITE_API_URL  ||'http://localhost:5002/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
